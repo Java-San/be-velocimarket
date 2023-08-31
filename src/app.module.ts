509e2f28cfb821d2af3a7fba195dedcf218516e1
AlfonsoContrerasS <140097097+AlfonsoContrerasS@users.noreply.github.com>
2023-08-30 21:25:57 -0400

    Revert "Feature/java"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductModule } from './product/product.module';
-import { CartModule } from './cart/cart.module';
-import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [ProductModule, CartModule, ReportsModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  console.log('Listen on port: 3000');
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
